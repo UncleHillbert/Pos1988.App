4abafdc59fdb8596dad78bb0b2f73a56d37be156
desarrollador25@dalton.com.mx <desarrollador25@dalton.com.mx>
2024-02-20 20:11:12 -0800

    Commit inicial primeras vistas y menu principal

---
+++ Data/Mesas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pos1988.Data
+{
+    public class Mesa
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public int QuantityPeople { get; set; }
+        public Order Order { get; set; }
+
+    }
+}
---
+++ Data/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace Pos1988.Data;
+
+public class Order
+{
+    public int Id { get; set; }
+    public DateTime Created { get; set; }
+
+    public List<OrderItem> Items { get; set; }
+    public decimal Price { get; set; }
+    public decimal Total { get; set; }
+}
